605840341b9dffd8ffde49fa1a16c942f393f41f
santosh <santoshacharya2468@gmail.com>
2021-05-08 19:57:03 +0545

    'payment'

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const app=express();
 app.get('/make_payment',(req,res)=>{
     res.sendFile(__dirname+'/payment.html');
 });
+app.get('/payment_sucess',(req,res)=>{
+    res.sendFile(__dirname+'/payment_sucess.html');
+});
+app.get('/payment_cancled',(req,res)=>{
+    res.sendFile(__dirname+'/payment_cancled.html');
+});
 app.listen(process.env.PORT || 3000,()=>{
     console.log('Server running');
 });
